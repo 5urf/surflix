1c11767b5aa215ff7ef5f57643c6e82e394a628a
LeeYunHak <sza0203@naver.com>
2025-04-01 19:43:39 +0900

    chore(초안): 프로젝트 초기 세팅

---
+++ src/Router.tsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from "react-router";
+import App from "./App";
+import ComingSoon from "./pages/ComingSoon";
+import Home from "./pages/Home";
+import NowPlaying from "./pages/NowPlaying";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "coming-soon",
+        element: <ComingSoon />,
+      },
+      {
+        path: "now-playing",
+        element: <NowPlaying />,
+      },
+    ],
+  },
+]);
+
+export default router;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router";
+import { ThemeProvider } from "styled-components";
+import router from "./Router";
+import { theme } from "./styles/theme";
 
-createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </QueryClientProvider>
+  </StrictMode>
+);
