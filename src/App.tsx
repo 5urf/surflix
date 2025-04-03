0fdd8dd3f30ae5da72c45b66f49226b321fc19f6
LeeYunHak <sza0203@naver.com>
2025-04-03 11:56:40 +0900

    fix(ThemeProvider): ThemeProvider 구조 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { Outlet } from "react-router";
+import { ThemeProvider } from "styled-components";
 import Header from "./components/header/Header";
+import { useThemeStore } from "./store/\bthemeStore";
 import { GlobalStyle } from "./styles/globalStyles";
+import { darkTheme, lightTheme } from "./styles/theme";
 
 function App() {
+  const { isDarkMode } = useThemeStore();
+  const theme = isDarkMode ? darkTheme : lightTheme;
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Header />
       <Outlet />
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,14 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
-import { ThemeProvider } from "styled-components";
 import router from "./Router";
-import { theme } from "./styles/theme";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <RouterProvider router={router} />
-      </ThemeProvider>
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </StrictMode>
 );
